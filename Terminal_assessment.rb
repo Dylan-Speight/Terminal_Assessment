5d7c786159342e07a742934370f6646ceacc638b
Grey-Feathers <d.speight92@gmail.com>
2019-03-07 10:37:09 +1000

    <insert commit message>

--- Terminal_assessment.rb
+++ Terminal_assessment.rb
@@ -1,53 +1,99 @@
-# A class for employers ie. cafe = Employer.new(position)
-# A class for employees ie. me = Employee.new(pay-rate, name)
-# produces a table that includes, hourly rate, age and name of employee, hours per week limit, hours available, total pay at the end of the week, employment type, etc.
-# Preferably using hashes instead of arrays
-# If it is possible to have to files communicating simultaneously like a message service?
-# The employee class should have methods to access, total money earned, pay for the week, pay per hour, possibly pay with and without tax deducted?, request hours, see schedule
-# The employer class should have a method to call on the timetable schedule as it stands and have complimentary methods to edit and manipulate it.
-class Employer
-    attr_accessor :name, :position, :age, :pay_rate, :hours_per_week_limit, :employment_type
-    def initialize(name, position, age,  pay_rate, hours_per_week_limit, employment_type)
-        @name = name
-        @position = position
-        @age = age
-        @pay_rate = "%.2f" % pay_rate
-        @hours_per_week_limit = hours_per_week_limit
-        @employment_type = employment_type
-    end
+require  'tty-table'                    # Tablemaker
+require 'io/console'                    # For the press any key method
 
-# class Employee < Employer
-#     def initialize(name, position, age, pay_rate, hours_per_week_limit, employment_type)
-#         super(name, position, age, pay_rate, hours_per_week_limit, employment_type)
-    # end
-    def about 
-        puts "#{@name} is a #{@employment_type} #{@position}." 
-        puts "They are #{@age}"
-        puts "They are currently paid $#{@pay_rate} an hour."
-        puts "They can work for a maximum of #{@hours_per_week_limit} hours a week."
+# Arrays
+table_headers = ["Name", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+employee_hours = [['James','n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a'], ['Tim','n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a' ], ['John','n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a']]
+awful_array_of_nothing = 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a'    ##### Gross yet effective #####
+# Variables
+employee_roster = 0
+name = 0
+day = 0
+day_array = 0
+name_remove = 0
+
+def continue                            # Press any key to continue function                                                                      
+    print "Press any key to continue"                                                                                                    
+    STDIN.getch                                                                                                              
+    print "            \r" # extra space to overwrite in case next sentence is short  
+    system('clear')                                                                                                            
+end  
+
+def get_roster(name,employee_hours)             # Retrieves the roster from within the nested array
+    employee_hours.each do |roster|
+        if name == roster[0]
+            return roster
+        end
     end
 end
-
-# employee_james = Employee.new("James", "Barista", "Casual", 16.00, 25)
-# employee_james.about
+def find_day(day, table_headers)
+    table_headers.index(day)
+end
+system('clear')
+puts "Welcome to rostr app"       # Smooth talking for the opener
+continue
 loop do
-puts "Welcome to the terminal roster app"
-puts "What is your new employees name?"
-name = gets.chomp.capitalize
-puts "What is their position?"
-position = gets.chomp.capitalize
-puts "Age"
-age = gets.chomp.to_i
-puts "How much ya paying them?"
-pay_rate = "%.2f" % gets.chomp.to_i
-puts "Employment type? eg. Casual, Part time, Full time"
-employment_type = gets.chomp.capitalize
-puts "What are their maximum hours per week?"
-hours_per_week_limit = gets.chomp.to_i
-new_employee = Employer.new(name, position, age, pay_rate, hours_per_week_limit, employment_type)
-new_employee.about
-break
+    system('clear')
+    puts "What would you like to do?"
+    puts "[E]dit (Roster), [C]reate (New employee), [D]isplay (Current Roster), [Q]uit"
+    menu_selection = gets.chomp.capitalize
+    case menu_selection
+    when "Edit", "E"                  # Creates or edits the current roster with any changes.
+        system('clear')
+        puts "Enter employee name"
+        name = gets.chomp.capitalize
+        employee_roster = get_roster(name,employee_hours)
+        
+        puts "Which day would you like to make changes to"
+        day = gets.chomp.capitalize 
+        day_array = table_headers.index(day)
+        
+        loop do
+            puts "Is #{name} working on #{day}? y/n"
+            employee_roster.delete_at(0)
+            employee_roster.insert(0, name)
+            day_working = gets.chomp.downcase
+            if day_working == "y"
+                puts "What time do they start?"
+                time_start = gets.chomp
+                puts "What time do they finish?"
+                time_end = gets.chomp
+                employee_roster.delete_at(day_array)
+                employee_roster.insert(day_array, "#{time_start} - #{time_end}")
+                break
+            elsif day_working == "n"
+                employee_hours.insert(day_array, "n/a")
+                break               
+            else 
+                puts "Invalid selection"
+            end        
+        end
+        
+    when "Create", "C"                       # Allows entry of a new employee into the roster
+        system('clear')
+        puts "What is the name of this newly acquired individual?"
+        employee_hours << awful_array_of_nothing.unshift(gets.chomp.capitalize)
+        
+    when "Display", "D"                     # Displays the current scheduled roster
+        system('clear')
+        puts "Your current roster is:"
+        table = TTY::Table.new table_headers, employee_hours
+        puts table.render(:ascii)
+        continue
+
+    when "Remove", "R"
+        puts "What is the name of the employee you are removing?"
+        name_remove = get_roster((gets.chomp.capitalize), employee_hours)
+        name_remove.delete
+        
+    when "Quit", "Q"                        # Quits out of the application
+        continue
+        system('clear')
+        break
+    else
+        puts "Invalid selection"
+    end
 end
-
-
-
+puts "Your completed roster is:"
+table = TTY::Table.new table_headers, employee_hours
+puts table.render(:ascii)
